6b621e85f83c033f3327710e10f653f25b39dee0
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 20:21:23 -0300

    356to54kecsacn

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -23,7 +23,9 @@ class SessionControler {
         name,
         email,
       },
-      token: jwt.sign({ id }, '996e573ac9e6a95e046ef731649fe0c9'),
+      token: jwt.sign({ id }, '996e573ac9e6a95e046ef731649fe0c9', {
+        expiresIn: '17d',
+      }),
     });
   }
 }
