cce529a4f6836f1071fbdcaa5aa3a265f7ab7430
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 20:20:06 -0300

    token- sucess

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,6 +11,20 @@ class SessionControler {
     if (!user) {
       return res.status(401).json({ error: 'Usuário não encontrado' });
     }
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'A senha não está correta' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, '996e573ac9e6a95e046ef731649fe0c9'),
+    });
   }
 }
 
