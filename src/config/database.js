7603dfe7fd41f72dd844a8a1d631c5ec92dfc9b0
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 16:04:33 -0300

    sequelize_config

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "docker",
+  database: "comigo",
+  define: {
+    timestamp: true,
+    underscoredAll: true,
+  },
+};
