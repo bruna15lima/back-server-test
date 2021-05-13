adc93298280311f2ffdb828a585e7f4a79b7b83d
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 18:52:47 -0300

    UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,13 +4,14 @@ class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    is (userExists)
+    is (userExists){
       return res.status(400).json({ error: 'User already exists.' });
     }
 
     const user = await User.create(req.body);
 
     return res.json(user);
+
   }
 }
 
