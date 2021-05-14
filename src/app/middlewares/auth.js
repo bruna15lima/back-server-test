c35e2043a72c0e5618881b217920546ecf26abc2
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 22:15:12 -0300

    ok

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,22 @@
-export default (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).json({ error: 'token not provided' });
+  }
 
-  return next();
+  const [, token] = authHeader.split('');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    console.log(decoded);
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'invalid token' });
+  }
 };
