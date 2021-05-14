3b0ba9bd7b4e10f0f01b2cfc9a4c155ad2ee6998
bruna calheiros <calheiros.bruna@gmail.com>
2021-05-13 21:32:11 -0300

    authMiddleware|Bearer

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.put('/users', authMiddleware, UserController.update);
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
